ec7c44db85af05162ac49290bb5899adc2d7b593
mia-im <62039441+mia-im@users.noreply.github.com>
2020-06-07 21:11:01 +0300

    Delete ch.php

--- ch.php
+++
@@ -1,7 +0,0 @@
-<?php
-$link = mysqli_connect('localhost', 'root' , '','lab');
-   
-$sql = mysqli_query($link, "UPDATE `users` SET `name` = '{$_POST['name']}',`surname` = '{$_POST['surname']}',`login` = '{$_POST['login']}', `password` = '{$_POST['password']}',`lang` = '{$_POST['lang']}',`role` = '{$_POST['role']}' WHERE `id`={$_GET['id']}");
-
-header("Location:list.php");
-?>
