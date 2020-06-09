34a11d3a8baa223fb2a38de325d9ec734d43717b
mia-im <62039441+mia-im@users.noreply.github.com>
2020-06-09 20:18:13 +0300

    Add files via upload

---
+++ actions/dop/users_buttons.php
@@ -0,0 +1,3 @@
+<?php include '../lang/language.php';?>
+<br><input type="button" value="<?php echo lang::trans('Users List'); ?>" onclick="location.href='../actions/list.php'"><br>
+<br><input type="button" value="<?php echo lang::trans('Log out'); ?>" onclick="location.href='../login/main.php'">
