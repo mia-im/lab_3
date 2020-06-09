6ac1aa87a80293d8365d91c4a975ad5a28b256ee
mia-im <62039441+mia-im@users.noreply.github.com>
2020-06-09 20:31:51 +0300

    Update 3.php

--- users/3.php
+++ users/3.php
@@ -1,5 +1,4 @@
 <?php
-session_start();
 require_once '../class.php';
 
 if ($_SESSION['role']!='3'){
@@ -17,7 +16,5 @@ else {
 	<br><a href="manager.php"><?php echo lang::trans('Manager information'); ?></a>
 	<br><a href="client.php"><?php echo lang::trans('Client information'); ?></a>
 </form>
-<?php include '../lang/language.php';?>
-<br><input type="button" value="<?php echo lang::trans('Users List'); ?>" onclick="location.href='../actions/list.php'"><br>
-<br><input type="button" value="<?php echo lang::trans('Log out'); ?>" onclick="location.href='../login/main.php'">
+<?php include '../actions/dop/users_buttons.php'; ?>
 
