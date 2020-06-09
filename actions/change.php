ca465e5bbc6a682a4ffb7490a441dca8ec7da56e
mia-im <62039441+mia-im@users.noreply.github.com>
2020-06-09 20:22:00 +0300

    Update change.php

--- actions/change.php
+++ actions/change.php
@@ -14,18 +14,7 @@ require_once "../class.php";
 </head>
 <body>
 <form name = "ch" action = "dop/ch.php?id=<? echo $_GET['id'];?>" method = "post" style="border: solid 1px; width: 400px;position: fixed; left: 35%;">
-    <label><p align="center">Login </label>
-    <p align="center"><input type = "text" name = "login">
-    <label><p align="center">Password </label>
-    <p align="center"><input type = "password" name = "password">
-    <label><p align="center">Name </label>
-    <p align="center"><input type = "text" name = "name">
-    <label><p align="center">Surname </label>
-    <p align="center"><input type = "text" name = "surname">
-    <label><p align="center">Language </label>
-    <p align="center"><input type = "text" name = "lang">
-    <label><p align="center">Role(3-admin, 2-manager, 1-client) </label>
-    <p align="center"><input type = "text" name = "role">
+    <?php include 'dop/add_ch_user.php'; ?>
     <p align="center"><button><?php echo lang::trans('Change'); ?></button>
 </form>
 </body>
