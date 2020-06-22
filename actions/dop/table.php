f2d978154fdf3829c85f94aad2f09592601b45b7
mia-im <62039441+mia-im@users.noreply.github.com>
2020-06-22 18:55:25 +0300

    Update table.php

--- actions/dop/table.php
+++ actions/dop/table.php
@@ -1,16 +1,16 @@
 <?php
- while ($result = mysqli_fetch_array($sql)) {
+ while ($result = mysqli_fetch_array($sql)) { ?>
 
-        echo '<tr>' .
-            "<td>{$result['id']}</td>" .
-            "<td>{$result['name']}</td>" .
-            "<td>{$result['surname']}</td>" .
-            "<td>{$result['login']}</td>" .
-            "<td>{$result['lang']}</td>" .
-            "<td>{$result['role']}</td>" .
-            "<td><a href='change.php?id=".$result['id']."'>Change</a></td>" .
-            "<td><a href='delete.php?id=".$result['id']."'>Delete</a></td>" .
-            '</tr>';
-    
-    }
+       <tr>
+            <td><?php echo $result['id']; ?></td>
+            <td><?php echo $result['name']; ?></td>
+            <td><?php echo $result['surname']; ?></td>
+            <td><?php echo $result['login']; ?></td>
+            <td><?php echo $result['lang']; ?></td>
+            <td><?php echo $result['role']; ?></td>
+            <td><a href='change.php?id=<?php echo $result['id']; ?>'>Change</a></td>
+            <td><a href='delete.php?id=<?php echo $result['id']; ?>'>Delete</a></td>
+            </tr>
+   <?php
+}
 ?>
